48a0edbd08cd74ce4ca2251afb87d36567283660
Victor Coelho <victor-matos8@hotmail.com>
2022-03-02 15:39:50 -0300

    Initial commit

---
+++ src/main/java/br/com/coelhovictor/springapibase/SpringApiBaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.coelhovictor.springapibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/coelhovictor/springapibase/SpringApiBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coelhovictor.springapibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
