98c060f57a6e9b0991ed7066733859736899a9f2
Victor Coelho <victor-matos8@hotmail.com>
2022-03-03 00:58:08 -0300

    Owner GET, POST, PUT and DELETE endpoints

---
+++ src/main/java/br/com/coelhovictor/springapibase/controllers/OwnerController.java
@@ -0,0 +1,71 @@
+package br.com.coelhovictor.springapibase.controllers;
+
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.coelhovictor.springapibase.domain.Owner;
+import br.com.coelhovictor.springapibase.dtos.OwnerDTO;
+import br.com.coelhovictor.springapibase.services.OwnerService;
+
+@RestController
+@RequestMapping(value = "/owners")
+public class OwnerController {
+
+	@Autowired
+	private OwnerService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Owner> find(@PathVariable Integer id) {
+		return ResponseEntity.ok(service.findById(id));
+	}
+	
+	@GetMapping()
+	public ResponseEntity<Page<Owner>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "5") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Owner> list = service.findAll(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok(list);
+	}
+
+	@PostMapping()
+	public ResponseEntity<Void> insert(@Valid @RequestBody OwnerDTO objDTO) {
+		Owner obj = service.fromDTO(objDTO);
+		service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@PathVariable Integer id, 
+			@Valid @RequestBody OwnerDTO objDTO) {
+		Owner obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+}
--- src/main/java/br/com/coelhovictor/springapibase/services/OwnerService.java
+++ src/main/java/br/com/coelhovictor/springapibase/services/OwnerService.java
@@ -1,10 +1,16 @@
 package br.com.coelhovictor.springapibase.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.coelhovictor.springapibase.domain.Owner;
+import br.com.coelhovictor.springapibase.dtos.OwnerDTO;
 import br.com.coelhovictor.springapibase.repositories.OwnerRepository;
+import br.com.coelhovictor.springapibase.services.exceptions.DataIntegrityException;
 import br.com.coelhovictor.springapibase.services.exceptions.NotFoundException;
 
 @Service
@@ -12,10 +18,52 @@ public class OwnerService {
 
 	@Autowired
 	private OwnerRepository repository;
+	
+	@Autowired
+	private CountryService countrySevice;
 
 	public Owner findById(Integer id) {
 		return repository.findById(id).orElseThrow(() -> 
 			new NotFoundException("Owner", id));
+	}
+	
+	public Page<Owner> findAll(Integer page, Integer linesPerPage,
+			String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, 
+				Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
+	
+	public void insert(Owner obj) {
+		obj.setId(null);
+		repository.save(obj);
+	}
+
+	public Owner update(Owner obj) {
+		Owner newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Cannot delete a owner "
+					+ "that has companies");
+		}
+	}
+	
+	public Owner fromDTO(OwnerDTO objDTO) {
+		return new Owner(null, objDTO.getName(), objDTO.getAge(), 
+				countrySevice.findById(objDTO.getCountryId()));
+	}
+	
+	private void updateData(Owner newObj, Owner obj) {
+		newObj.setName(obj.getName());
+		newObj.setAge(obj.getAge());
+		newObj.setCountry(obj.getCountry());
 	}
 	
 }
