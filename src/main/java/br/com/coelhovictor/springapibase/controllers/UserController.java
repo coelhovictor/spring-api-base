b4897dfd45beafa4f6bf05481a4bc80217558a2a
Victor Coelho <victor-matos8@hotmail.com>
2022-03-03 14:30:05 -0300

    User GET, POST, PUT and DELETE endpoints

---
+++ src/main/java/br/com/coelhovictor/springapibase/controllers/UserController.java
@@ -0,0 +1,73 @@
+package br.com.coelhovictor.springapibase.controllers;
+
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.coelhovictor.springapibase.domain.User;
+import br.com.coelhovictor.springapibase.dtos.UserDTO;
+import br.com.coelhovictor.springapibase.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+@PreAuthorize("hasAnyRole('ADMIN')")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> find(@PathVariable Integer id) {
+		return ResponseEntity.ok(service.findById(id));
+	}
+
+	@GetMapping()
+	public ResponseEntity<Page<User>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "5") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "createdAt") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<User> list = service.findAll(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok(list);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Void> insert(@Valid @RequestBody UserDTO objDTO) {
+		User obj = service.fromDTO(objDTO);
+		service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@PathVariable Integer id, 
+			@Valid @RequestBody UserDTO objDTO) {
+		User obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+}
--- src/main/java/br/com/coelhovictor/springapibase/services/UserService.java
+++ src/main/java/br/com/coelhovictor/springapibase/services/UserService.java
@@ -3,6 +3,10 @@ package br.com.coelhovictor.springapibase.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,6 +15,8 @@ import br.com.coelhovictor.springapibase.domain.User;
 import br.com.coelhovictor.springapibase.dtos.UserDTO;
 import br.com.coelhovictor.springapibase.repositories.UserRepository;
 import br.com.coelhovictor.springapibase.services.exceptions.ConflictException;
+import br.com.coelhovictor.springapibase.services.exceptions.DataIntegrityException;
+import br.com.coelhovictor.springapibase.services.exceptions.NotFoundException;
 
 @Service
 public class UserService {
@@ -20,6 +26,18 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository repository;
+	
+	public User findById(Integer id) {
+		return repository.findById(id).orElseThrow(() -> 
+			new NotFoundException("User", id));
+	}
+	
+	public Page<User> findAll(Integer page, Integer linesPerPage,
+			String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, 
+				Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
 	
 	public void register(User obj) {
 		if(authenticated() != null) 
@@ -34,10 +52,36 @@ public class UserService {
 		repository.save(obj);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		User currentUser = authenticated();
+		if(currentUser.getId() == id)
+			throw new ConflictException("You can't delete yourself");
+		
+		findById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Cannot delete this user");
+		}
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(null, objDTO.getName(), objDTO.getUsername(), objDTO.getEmail(),
 				passwordEncoder.encode(objDTO.getPassword()), objDTO.getBirthday(),
 				new Date(System.currentTimeMillis()));
+	}
+	
+	private void updateData(User newObj, User obj) {
+		//can't update username and email
+		newObj.setName(obj.getName());
+		newObj.setPassword(obj.getPassword());
+		newObj.setBirthday(obj.getBirthday());
 	}
 	
 	public static User authenticated() {
@@ -48,4 +92,5 @@ public class UserService {
 			return null;
 		}
 	}
+	
 }
