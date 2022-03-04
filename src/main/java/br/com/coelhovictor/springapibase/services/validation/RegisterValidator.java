be204e66cac58c6c5ec5741e586b6ea604f635d3
Victor Coelho <victor-matos8@hotmail.com>
2022-03-04 18:41:28 -0300

    Username characters validation

--- src/main/java/br/com/coelhovictor/springapibase/services/validation/RegisterValidator.java
+++ src/main/java/br/com/coelhovictor/springapibase/services/validation/RegisterValidator.java
@@ -12,12 +12,11 @@ import br.com.coelhovictor.springapibase.controllers.exceptions.FieldMessage;
 import br.com.coelhovictor.springapibase.dtos.RegisterDTO;
 import br.com.coelhovictor.springapibase.repositories.UserRepository;
 
-public class RegisterValidator implements 
-	ConstraintValidator<RegisterValid, RegisterDTO> {
-	
+public class RegisterValidator implements ConstraintValidator<RegisterValid, RegisterDTO> {
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	@Override
 	public void initialize(RegisterValid ann) {
 	}
@@ -25,25 +24,40 @@ public class RegisterValidator implements
 	@Override
 	public boolean isValid(RegisterDTO objDTO, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-		
+
 		try {
-			if(repository.findByEmailIgnoreCase(objDTO.getEmail()) != null)
-				list.add(new FieldMessage("name", "A user with that "
-						+ "email already exists"));
-			
-			if(repository.findByUsernameIgnoreCase(objDTO.getUsername()) != null)
-				list.add(new FieldMessage("name", "A user with that "
-						+ "username already exists"));
+			if (!validUsername(objDTO.getUsername()))
+				list.add(new FieldMessage("username", "Has invalid characters"));
+
+			if (repository.findByEmailIgnoreCase(objDTO.getEmail()) != null)
+				list.add(new FieldMessage("email", "A user with that " + "email already exists"));
+
+			if (repository.findByUsernameIgnoreCase(objDTO.getUsername()) != null)
+				list.add(new FieldMessage("name", "A user with that " + "username already exists"));
 		} catch (NullPointerException e) {
 			return false;
 		}
-		
-		for(FieldMessage fieldMessage : list) {
+
+		for (FieldMessage fieldMessage : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(fieldMessage.getMessage())
-				.addPropertyNode(fieldMessage.getFieldName()).addConstraintViolation();
+					.addPropertyNode(fieldMessage.getFieldName()).addConstraintViolation();
 		}
 		return list.isEmpty();
 	}
-	
+
+	private boolean validUsername(String username) {
+		boolean hasLetters = false;
+		for (int i = 0; i < username.length(); i++) {
+			int ch = (int)username.charAt(i);
+			
+			if((ch >= 65 && ch <= 90) || 
+					(ch >= 97 && ch <= 122))
+				hasLetters = true;
+			else if(!(ch >= 48 && ch <= 57))
+				return false;
+		}
+		return hasLetters;
+	}
+
 }
